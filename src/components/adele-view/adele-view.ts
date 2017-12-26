5024f04f64104d0f651afe2f1818d3fb5cfac959
Alex Vargas Benamburg <vb.alexr@gmail.com>
2017-12-25 21:40:56 -0600

    [UPDATE] Empleo de clase Comando.

--- src/components/adele-view/adele-view.ts
+++ src/components/adele-view/adele-view.ts
@@ -1,5 +1,6 @@
 import { Component,ViewChild } from '@angular/core';
 
+import { Comando } from './comando';
 
 @Component({
   selector: 'AdeleView',
@@ -7,27 +8,37 @@ import { Component,ViewChild } from '@angular/core';
 })
 export class AdeleViewComponent {
 
-  @ViewChild('adeleCanvas') adeleCanvas;
+  @ViewChild('adeleCanvas') private adeleCanvas;
+
+  private arriba:Comando;
 
   constructor() {
   }
 
   ngAfterViewInit(){
+    let ctx = this.adeleCanvas.nativeElement.getContext('2d');
+
+    this.arriba = new Comando(ctx,"a","assets/adele/arrowU.png");
+    this.arriba.setTamanio([10,20]);
+    
     this.adeleCanvas.nativeElement.width  = window.innerWidth;
     this.adeleCanvas.nativeElement.height = window.innerHeight;
     let hammer = new window['Hammer'](this.adeleCanvas.nativeElement);
     hammer.get('pan').set({ direction: window['Hammer'].DIRECTION_ALL });
     hammer.on('pan', (ev) => {
-      let ctx = this.adeleCanvas.nativeElement.getContext('2d');
+      
       ctx.clearRect(0, 0, this.adeleCanvas.nativeElement.width, this.adeleCanvas.nativeElement.height);
-      ctx.beginPath();
-      ctx.arc(ev.pointers[0].clientX, ev.pointers[0].clientY, 15, 0, 2 * Math.PI, true);
-      ctx.fillStyle = 'black';
-      ctx.fill();
+
+      let x = ev.pointers[0].clientX;
+      let y = ev.pointers[0].clientY;
+
+      this.arriba.setPosicion([x,y]);
+      this.arriba.dibujar();
+
       console.log(ev);
       
     });
+  }
 
-}
 
 }
