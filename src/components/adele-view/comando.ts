f0ec0d6da3a507aa2cc9fe8e742fe9c2968cc6c5
Alex Vargas Benamburg <vb.alexr@gmail.com>
2017-12-25 21:40:32 -0600

    [NEW] Clase para uso generico de comandos agregada.

---
+++ src/components/adele-view/comando.ts
@@ -0,0 +1,72 @@
+
+export class Comando{
+    private imagen:HTMLImageElement;
+    private esDibujable:boolean;
+
+    private posX:number;
+    private posY:number;
+
+    private tamX:number;
+    private tamY:number;
+
+    constructor(
+        private contexto:CanvasRenderingContext2D,
+        private comando:string,
+        private imagenSrc:string
+    ){
+        this.esDibujable = false;
+        this.tamX = this.tamY = null;
+
+        this.imagen = new Image();
+        this.imagen.onload = ()=>{
+            this.esDibujable = true;
+            if(this.tamY===null){
+                this.tamX = this.imagen.width;
+                this.tamY = this.imagen.height; 
+            }
+        }
+        this.imagen.src = this.imagenSrc;
+    }
+
+    public setPosicion(pos:[number,number]):void{
+        this.posX = pos[0];
+        this.posY = pos[1];
+    }
+
+    public getPosicion():[number,number]{
+        return [this.posX,this.posY];
+    }
+
+    public setTamanio(tamanio:[number,number]):void{
+        this.tamX = tamanio[0];
+        this.tamY = tamanio[1];
+    }
+
+    public getTamanio():[number,number]{
+        return [this.tamX,this.tamY];
+    }
+
+    public getComando():string{
+        return this.comando;
+    }
+
+    public dibujar():void{
+        if(this.esDibujable){
+            this.contexto.drawImage(this.imagen,this.posX-(this.tamX/2),this.posY-(this.tamY/2),this.tamX,this.tamY);
+        }
+    }
+
+    public estaDentro(coordenadas:[number,number]):boolean{
+        let esta:boolean = false;
+        if(
+            coordenadas[0] > this.posX && 
+            coordenadas[0]< (this.posX+this.tamX) && 
+            coordenadas[1] > this.posY && 
+            coordenadas[1] < (this.posY+this.tamY))
+            {
+                esta = true;
+        }
+        return esta;
+    }
+
+}
