b87ccc0cd2acbd1910a37290b3765b2b77fbbaba
Alex Vargas Benamburg <vb.alexr@gmail.com>
2017-12-28 09:54:46 -0600

    [NEW] Agregada documentacion interna [NEW] Agregados eventos onPlay y onConfig [UPDATE] Creada fucion para automatizar comandos agregados [UPDATE] Tamanio de graficos definidos por % de ancho

--- src/components/adele-view/adele-view.ts
+++ src/components/adele-view/adele-view.ts
@@ -1,4 +1,4 @@
-import { Component,ViewChild } from '@angular/core';
+import { Component,ViewChild,Output,EventEmitter,NgZone } from '@angular/core';
 
 import { Comando } from './comando';
 
@@ -8,33 +8,71 @@ import { Comando } from './comando';
 })
 export class AdeleViewComponent {
 
+  // Canvas sobre el que se trabaja
   @ViewChild('adeleCanvas') private adeleCanvas;
 
+  // Logo de adele, lanzador de onConfig
   private logo:Comando;
+  // Boton de play, lanzador de onPlay
   private play:Comando;
+  // Boton de erase, ejecuta un undo
   private erase:Comando;
+  
+  // Contador para posicionamiento
+  private aPos:number;
 
+  // Lista de comandos disponibles
   private comandos:Array<Comando>;
+  // Lista de comandos a ejecutar (instruciones para adele)
   private programa:Array<Comando>;
 
-  constructor() {
+  @Output() // Emisor de eventos para el onPlay
+  private onPlay:EventEmitter<Array<string>> = new EventEmitter<Array<string>>();
+  @Output() // Emisor de eventos para el onConfig
+  private onConfig:EventEmitter<void> = new EventEmitter<void>();
+
+  /**
+   * Constructor por defecto de la clase
+   */
+  constructor(private ngZone:NgZone) {
     this.comandos = new Array<Comando>();
     this.programa = new Array<Comando>();
+    window.onresize = (e) =>{
+        //ngZone.run will help to run change detection
+        this.ngZone.run(() => {
+            this.init();
+        });
+    };
   }
 
-    ngAfterViewInit(){
+  /**
+   * Wraper de on ready
+   */
+  ngAfterViewInit(){
+    this.init();
+  }
+  /**
+   * Funcion para inicializar todo el componente
+   */
+  private init():void{
     let ctx = this.adeleCanvas.nativeElement.getContext('2d');
     this.configCanvas(ctx);
     this.initComandos(ctx);
   }
 
+  /**
+   * Funcion para configurar el canvas; dimeciones y eventos sobre el mismo
+   * @param ctx Contexto del canvas al configurar
+   */
   private configCanvas(ctx:CanvasRenderingContext2D):void {
     this.adeleCanvas.nativeElement.width  = window.innerWidth;
     this.adeleCanvas.nativeElement.height = window.innerHeight;
     
     let c:Comando = null;
     let hammer = new window['Hammer'](this.adeleCanvas.nativeElement);
-    
+    hammer.get('pan').set({ direction: window['Hammer'].DIRECTION_ALL });
+
+    //Manejo de toques sobre los botones de accion
     hammer.on('tap',(ev)=>{
       let x = ev.pointers[0].clientX;
       let y = ev.pointers[0].clientY;
@@ -44,37 +82,44 @@ export class AdeleViewComponent {
         this.redibujar(ctx);
       }
       if(this.play.estaDentro([x,y])){
-        //TODO play
+        this.emitPlay();
       }
       if(this.logo.estaDentro([x,y])){
-        //TODO config
+        this.emitConfig();
       }
     });
 
-    hammer.get('pan').set({ direction: window['Hammer'].DIRECTION_ALL });
+    // Manejo de comienzos de arrastres.
     hammer.on('panstart',(ev)=>{
       let x = ev.pointers[0].clientX;
       let y = ev.pointers[0].clientY;
       console.info("Iniciando drag: ",x,y);
+      // Si el arrastre comienza en los comandos clonar y 
+      // agregar a instrucciones de adele y seleciona el objeto
       for (let i=0;i<this.comandos.length && c==null;++i) {
         if(this.comandos[i].estaDentro([x,y])){
           c = this.comandos[i].clone();
           this.programa.push(c);
         }
       }
+      // Si el arrastre comienza en las instrucciones solo
+      // seleciona el objeto
       for (let i=0;i<this.programa.length && c==null;++i) {
         if(this.programa[i].estaDentro([x,y])){
           c = this.programa[i];
         }
       }
     });
+    // Manejo de arrastre
     hammer.on('pan', (ev) => {
       let x = ev.pointers[0].clientX;
       let y = ev.pointers[0].clientY;
-      if(c!=null){
+      // si el arrastre comenzo sobre algun objeto moverlo
+      if(c!=null){ 
         c.setPosicion([x,y]);
         this.redibujar(ctx);
       }
+      //Si se esta terminando el arrastre deselecionar el objeto
       if(ev.isFinal){
         console.info("Terminando drag: ",x,y);
         c=null;
@@ -83,68 +128,59 @@ export class AdeleViewComponent {
   }
 
   private initComandos(ctx:CanvasRenderingContext2D):void {
-    this.logo = new Comando(ctx,"","assets/adele/adeleLogo.png",true);
-    this.logo.setPosicion([55,window.innerHeight-20]);
+    let width  = window.innerWidth;
+    let tamanio = width*0.075;// 5% de tamanio 
+    
+
+    //Botones de funcionamiento
+    this.logo = new Comando(ctx,"config","assets/adele/adeleLogo.png",true);
+    this.logo.setPosicion([tamanio+50,window.innerHeight-tamanio]);
     
     this.play = new Comando(ctx,"play","assets/adele/play.png",true);
-    this.play.setTamanio([30,30]);
-    this.play.setPosicion([window.innerWidth-40,30]);
+    this.play.setTamanio([tamanio,tamanio]);
+    this.play.setPosicion([window.innerWidth-(tamanio+10),tamanio]);
     
     this.erase = new Comando(ctx,"erase","assets/adele/erase.png",true);
-    this.erase.setTamanio([30,30]);
-    this.erase.setPosicion([window.innerWidth-40,window.innerHeight-20]);
+    this.erase.setTamanio([tamanio,tamanio]);
+    this.erase.setPosicion([window.innerWidth-(tamanio+10),window.innerHeight-(tamanio)]);
+    
+    this.aPos = width*0.1;
+
+    //Comandos de adele
+    this.addComando(new Comando(ctx,"u","assets/adele/arrowU.png",true));
+    this.addComando(new Comando(ctx,"d","assets/adele/arrowD.png",true));
+    this.addComando(new Comando(ctx,"l","assets/adele/arrowL.png",true));
+    this.addComando(new Comando(ctx,"r","assets/adele/arrowR.png",true));
+    this.addComando(new Comando(ctx,"c","assets/adele/clawC.png",true));
+    this.addComando(new Comando(ctx,"o","assets/adele/clawO.png",true));
+    this.addComando(new Comando(ctx,"q","assets/adele/color.png",true));
+    this.addComando(new Comando(ctx,"m","assets/adele/note.png",true));
     
 
-    //botones
-    let temp= new Comando(ctx,"u","assets/adele/arrowU.png",true);
-    temp.setTamanio([30,30]);
-    temp.setPosicion([140,30]);
-    this.comandos.push(temp);
+  }
+  /**
+   * Funcion para agregar un comando de forma ordenada
+   * @param c Comando a agregar
+   */
+  private addComando(c:Comando):void{
+    let width  = window.innerWidth;
+    let space = width*0.025;// 2.5% del ancho para espacio entre comados.
+    let tamanio = width*0.05;// 5% del ancho para el tamanio de los comandos
 
-    temp= new Comando(ctx,"d","assets/adele/arrowD.png",true);
-    temp.setTamanio([30,30]);
-    temp.setPosicion([180,30]);
-    this.comandos.push(temp);
-
-    temp= new Comando(ctx,"l","assets/adele/arrowL.png",true);
-    temp.setTamanio([30,30]);
-    temp.setPosicion([220,30]);
-    this.comandos.push(temp);
-
-
-    temp= new Comando(ctx,"r","assets/adele/arrowR.png",true);
-    temp.setTamanio([30,30]);
-    temp.setPosicion([260,30]);
-    this.comandos.push(temp);
-
-    temp= new Comando(ctx,"c","assets/adele/clawC.png",true);
-    temp.setTamanio([30,30]);
-    temp.setPosicion([300,30]);
-    this.comandos.push(temp);
-
-    temp= new Comando(ctx,"o","assets/adele/clawO.png",true);
-    temp.setTamanio([30,30]);
-    temp.setPosicion([340,30]);
-    this.comandos.push(temp);
-
-    temp= new Comando(ctx,"q","assets/adele/color.png",true);
-    temp.setTamanio([30,30]);
-    temp.setPosicion([380,30]);
-    this.comandos.push(temp);
-
-    temp= new Comando(ctx,"q","assets/adele/note.png",true);
-    temp.setTamanio([30,30]);
-    temp.setPosicion([420,30]);
-    this.comandos.push(temp);
-
+    c.setTamanio([tamanio,tamanio]);
+    c.setPosicion([this.aPos,tamanio]);
+    this.comandos.push(c);
+    this.aPos = this.aPos+tamanio+space;
   }
 
+  /**
+   * Funcion para redibujar todos los componentes
+   * @param ctx Contexto a redibujar
+   */
   private redibujar(ctx:CanvasRenderingContext2D):void{
     ctx.clearRect(0, 0, this.adeleCanvas.nativeElement.width, this.adeleCanvas.nativeElement.height);
     this.logo.dibujar();
-    for (let i=0;i<this.comandos.length;++i) {
-      this.comandos[i].dibujar();
-    }
+   
     for (let i=0;i<this.programa.length;++i) {
       this.programa[i].dibujar();
       if(i<(this.programa.length-1)){
@@ -165,8 +201,31 @@ export class AdeleViewComponent {
         ctx.stroke();
       }
     }
+    for (let i=0;i<this.comandos.length;++i) {
+      this.comandos[i].dibujar();
+    }
     this.play.dibujar();
     this.erase.dibujar();
+  }
+
+  /**
+   * Funcion para emitir evento onPlay
+   */
+  private emitPlay():void{
+    let as = new Array<string>();
+    for(let i=0; i<this.programa.length;++i){
+      as.push(this.programa[i].getComando());
+    }
+    console.info("Event On Play: ",as);
+    this.onPlay.emit(as);
+  }
+
+  /**
+   * Funcion para emitir evento onConfig
+   */
+  private emitConfig():void{
+    console.info("Event On Config");
+    this.onConfig.emit();
   }
 
 }
