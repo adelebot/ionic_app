817f5423671e20af94056f530c55adc92f9776e1
Alex Vargas Benamburg <vb.alexr@gmail.com>
2018-01-04 20:31:59 -0600

    [UPDATE] Agregado logica al controlador de la vista de configuracion

--- src/pages/config/config.ts
+++ src/pages/config/config.ts
@@ -1,17 +1,54 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController,LoadingController } from 'ionic-angular';
+
+import { ComunicacionProvider } from '../../providers/comunicacion/comunicacion';
 
 @Component({
   selector: 'page-config',
   templateUrl: 'config.html',
 })
 export class ConfigPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    private navCtrl: NavController,
+    private comunicacion:ComunicacionProvider,
+    private loadingCtrl: LoadingController
+  ) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ConfigPage');
+  private activar():void{
+    this.comunicacion.enable();
   }
 
+  private buscar():void{
+    let animacion = this.loadingCtrl.create({
+      content: 'Buscando nuevos dispositivos'
+    });
+    animacion.present();
+    this.comunicacion.find().then(()=>{
+      animacion.dismiss();
+    }).catch(()=>{
+      animacion.dismiss();
+    })
+  }
+
+  private regresar():void{
+    this.navCtrl.pop();
+  }
+
+  private conectar(dispositivo:any){
+    let animacion = this.loadingCtrl.create({
+      content: 'Conectando'
+    });
+    animacion.present();
+    this.comunicacion.connect(dispositivo).then(()=>{
+      this.navCtrl.pop();       
+      animacion.dismiss();
+    }).catch(()=>{
+      animacion.dismiss();
+    });
+  }
+
+  private desconectar():void{
+    this.comunicacion.disconnect();
+  }
 }
